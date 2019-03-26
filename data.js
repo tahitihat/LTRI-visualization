12290d799dfb3c8d9b025323a5891cb810e69818
rishmathur21 <mathur.ri@husky.neu.edu>
2019-03-25 23:41:41 -0400

    Read in data from CSVs

---
+++ data.js
@@ -0,0 +1,14 @@
+d3.csv("./Data/LegalIndicators.csv", function(legal_data) {
+  console.log(legal_data);
+});
+
+d3.csv("./Data/Question45.csv", function(q45_data) {
+  console.log(q45_data);
+});
+
+
+d3.csv("./Data/Question46.csv", function(q46_data) {
+  console.log(q46_data);
+});
+
+
