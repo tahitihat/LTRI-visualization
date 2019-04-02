b147ac1a4eb5fe1b108e14d7605aadb6a1f0d254
Isabel Tripp <stagestruck1@gmail.com>
2019-04-01 20:38:56 -0400

    variable name change

--- violin.js
+++ violin.js
@@ -1,85 +1,81 @@
-// <!-- Create a div where the graph will take place -->
+var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
+// append svg object to page
+var svg = d3.select("#violin")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
-// Read the data and compute summary statistics for each specie
-d3.csv("./data/survey/AdjustedQuestion45.csv", function(data) {
+// Read in survey data
+d3.csv("./data/survey/AdjustedQuestion45.csv", function (data) {
 
-  // Build and Show the Y scale
+  // Build y scale
   var y = d3.scaleLinear()
-    .domain([ 0,7 ])          // Note that here the Y scale is set manually
+    .domain([0, 7])
     .range([height, 0])
-  svg.append("g").call( d3.axisLeft(y) )
+  svg.append("g").call(d3.axisLeft(y))
 
-  // Build and Show the X scale. It is a band scale like for a boxplot: each group has an dedicated RANGE on the axis. This range has a length of x.bandwidth
+  // Build x scale
   var x = d3.scaleBand()
-    .range([ 0, width ])
+    .range([0, width])
     .domain(["Burkina_Faso", "Cameroon", "Costa_Rica"])
-    .padding(0.05)     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
+    .padding(0.05)
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
 
-  // Features of the histogram
+  // Histogram
   var histogram = d3.histogram()
-        .domain(y.domain())
-        .thresholds(y.ticks(20))    // Important: how many bins approx are going to be made? It is the 'resolution' of the violin plot
-        .value(d => d)
+    .domain(y.domain())
+    .thresholds(y.ticks(20)) // resolution
+    .value(d => d)
 
   // Compute the binning for each group of the dataset
   var sumstat = d3.nest()  // nest function allows to group the calculation per level of a factor
-    .key(function(d) {
-      // console.log(d.country);
-      return d.country;})
-    .rollup(function(d) {   // For each key..
-      input = d.map(function(g) { return g.response;})    // Keep the variable called response
-      bins = histogram(input)   // And compute the binning on it.
-      return(bins)
+    .key(function (d) {
+      return d.country;
+    })
+    .rollup(function (d) {
+      input = d.map(function (g) { return g.response; })
+      bins = histogram(input)
+      return (bins)
     })
     .entries(data)
 
-  // What is the biggest number of value in a bin? We need it cause this value will have a width of 100% of the bandwidth.
+  // What is the biggest number of value in a bin? 
   var maxNum = 0
-  for ( i in sumstat ){
+  for (i in sumstat) {
     allBins = sumstat[i].value
-    lengths = allBins.map(function(a){return a.length;})
-    longuest = d3.max(lengths)
-    if (longuest > maxNum) { maxNum = longuest }
+    lengths = allBins.map(function (a) { return a.length; })
+    longest = d3.max(lengths)
+    if (longest > maxNum) { maxNum = longest }
   }
 
   // The maximum width of a violin must be x.bandwidth = the width dedicated to a group
   var xNum = d3.scaleLinear()
     .range([0, x.bandwidth()])
-    .domain([-maxNum,maxNum])
-
+    .domain([-maxNum, maxNum])
 
   // Add the shape to this svg!
   svg
     .selectAll("myViolin")
     .data(sumstat)
-    .enter()        // So now we are working group per group
+    .enter()
     .append("g")
-      .attr("transform", function(d){ return("translate(" + x(d.key) +" ,0)") } ) // Translation on the right to be at the group position
+    .attr("transform", function (d) { return ("translate(" + x(d.key) + " ,0)") }) // Translation on the right to be at the group position
     .append("path")
-        .datum(function(d){ return(d.value)})     // So now we are working bin per bin
-        .style("stroke", "none")
-        .style("fill","#69b3a2")
-        .attr("d", d3.area()
-            .x0(function(d){ return(xNum(-d.length)) } )
-            .x1(function(d){ return(xNum(d.length)) } )
-            .y(function(d){ return(y(d.x0)) } )
-            .curve(d3.curveCatmullRom)    // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
-        )
+    .datum(function (d) { return (d.value) })
+    .style("stroke", "none")
+    .style("fill", "#69b3a2")
+    .attr("d", d3.area()
+      .x0(function (d) { return (xNum(-d.length)) })
+      .x1(function (d) { return (xNum(d.length)) })
+      .y(function (d) { return (y(d.x0)) })
+      .curve(d3.curveCatmullRom)
+    )
 });
